d5ebee2497cbf398d4d36048322bc525c191406a
= <=>
2022-02-03 16:25:19 -0400

    reparando migraciones

--- db/migrations/20220202165728-create-orders-table.js
+++ db/migrations/20220202165728-create-orders-table.js
@@ -4,7 +4,30 @@ const { orderSchema, ORDER_TABLE } = require('../models/orders.model');
 
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, orderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        unique: true,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        }
+      },
+    });
   },
 
   async down (queryInterface) {
