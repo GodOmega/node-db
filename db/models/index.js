2aee93b48b9430d3590a0d9e0f90297840838428
= <=>
2022-02-03 11:57:47 -0400

    agregando relacion orders y products con items

--- db/models/index.js
+++ db/models/index.js
@@ -2,6 +2,8 @@ const { User, UserSchema } = require('./user.model')
 const { Customer, CustomerSchema } = require('./customer.model')
 const { Category, categorySchema } = require('./category.model')
 const { Product, productSchema } = require('./product.model')
+const { Order, orderSchema } = require('./orders.model')
+const { OrderProduct, orderProductSchema} = require('./order-product.model')
 
 function setupModels(sequelize) {
   // Initialize models
@@ -9,12 +11,15 @@ function setupModels(sequelize) {
   Customer.init(CustomerSchema, Customer.config(sequelize));
   Category.init(categorySchema, Category.config(sequelize));
   Product.init(productSchema, Product.config(sequelize));
+  Order.init(orderSchema, Order.config(sequelize));
+  OrderProduct.init(orderProductSchema, OrderProduct.config(sequelize));
 
   // Relationships
   User.associate(sequelize.models);
   Customer.associate(sequelize.models);
-  Category.associate(sequelize.models)
-  Product.associate(sequelize.models)
+  Category.associate(sequelize.models);
+  Product.associate(sequelize.models);
+  Order.associate(sequelize.models);
 }
 
 module.exports = setupModels;
